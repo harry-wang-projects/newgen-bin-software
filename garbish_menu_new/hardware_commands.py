442b5dc542a8ea5be9e9b72dceee3e25f37fbf64
root <wa6402@gmail.com>
2023-05-01 22:27:32 +0800

    hardware working on pi

---
+++ garbish_menu_new/hardware_commands.py
@@ -0,0 +1,57 @@
+import serial
+from time import sleep
+
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+ser.flush()
+
+ser.write(b'R')
+
+def unlock():
+    ser.write(b'U')
+    ser.flush()
+    sleep(0.5)
+    triggered = False
+    while True:
+        if ser.in_waiting > 0:
+            while ser.in_waiting > 0:
+                line = ser.readline().decode().rstrip()
+
+            main = line.split("[", 3)
+            print(main)
+ 
+            lhs = (main[len(main) - 1].split(" ", 3))
+            rhs = (main[len(main) - 1].split(" ", 3))
+            print('point2')
+            print(lhs)
+            print('point3')
+            if int(lhs[1]) == 0:
+                print('true!!')
+                return
+                 
+        
+
+def lock_state():
+    line = ''
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            break
+
+        main = line.split("[", 2)
+
+        lhs = main[0].split(" ", 3)
+        return int(lhs[1])
+
+def get_weight():
+    line = ''
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            break
+        
+        main = line.split("[", 1)       
+ 
+        lhs = main.split(" ", 1)
+        return float(lhs)
+
+
